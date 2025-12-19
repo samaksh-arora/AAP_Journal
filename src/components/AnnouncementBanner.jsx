b4f9d726ed4cb5f3a00e7e0504f9ad5a5cba3d60
Samaksh Arora <samaksharora.09@gmail.com>
2025-12-19 12:06:52 -0500

    Update announcement date in AnnouncementBanner component

--- src/components/AnnouncementBanner.jsx
+++ src/components/AnnouncementBanner.jsx
@@ -3,7 +3,7 @@ import './AnnouncementBanner.css'
 function AnnouncementBanner() {
   return (
     <div className="announcement-banner">
-      📢 Volume I, Issue I coming December 15th, 2025!
+      📢 Volume I, Issue I coming December 19th, 2025!
     </div>
   )
 }
